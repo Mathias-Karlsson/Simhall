9fadeeed7f0c3c440ce6a9f79ce81f74e6bf1d78
mathkarl <mathkarl@LSE-B2725>
2020-01-29 13:05:06 +0000

    Add project files.

---
+++ Simhall/Swimmer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhall
+{
+    class Swimmer
+    {
+        private string name;
+
+        private BathingSuit suit;
+
+        public string Swim()
+        {
+            return "Splish Splash - it's so fun in the bath!";
+        }
+
+        public void SetSuit(BathingSuit)
+        {
+
+        }
+    }
+}
